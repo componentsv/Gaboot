6811ee8703f42534701c5c37a44720031ac72840
rn <associate.ru@outlook.com>
2017-05-15 16:28:56 +0700

    Modified Resources/views/layouts/master

--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Component Gaboot</title>
+        <title>Oauth2</title>
 
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script>
@@ -16,6 +16,6 @@
             @yield('content')
         </div>
 
-        <script src="./../js/app.js"></script>
+        <script src="{{asset('js/auth.js')}}"></script>
     </body>
 </html>
